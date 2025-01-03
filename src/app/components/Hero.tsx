1df137bc2cac0b84214c4b537008d16d96227f6d
Joshua Owens <38735622+joshbeme@users.noreply.github.com>
2025-01-02 20:55:20 -0800

    Fun Display for company.

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,18 +1,44 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import HeroImage from "@src/assets/8-bit-josh.png";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 interface HeroProps {
   title: string;
   subtitle: string;
 }
 
+const CompanyTag = ({ onClick }: { onClick: () => void }) => {
+  const currentPath = usePathname() || "";
+  const currentCompany = currentPath.split("/")[1];
+
+  return (
+    <a
+      onClick={onClick}
+      style={{ display: "inline-block" }}
+      className="absolute nes-pointer inline-block text-sm left-[52%] bottom-[40vw] sm:bottom-1/3 z-50 nes-balloon from-left"
+    >
+      Hello {currentCompany}!
+    </a>
+  );
+};
+
 const Hero: React.FC<HeroProps> = ({ title, subtitle }) => {
+  const currentPath = usePathname() || "";
+  const currentCompany = currentPath.split("/")[1] || "";
+  const [textBubble, setTextBubble] = useState(currentCompany.length > 1);
+  const closeBubble = () => {
+    setTextBubble(false);
+  };
   return (
     <section className="relative flex flex-col flex-center content-center items-center ">
       <div className="pt-32 text-4xl relative nes-container flex justify-center items-start w-full h-[75vh] bg-[url(/HeroImage1.png)] bg-center bg-cover overflow-hidden">
         {/* place text here if you want for a hero title */}
-        <div className="nes-container bg-white/60 border-s-black rounded-full absolute -bottom-10 sm:-bottom-12 overflow-hidden max-w-[360px] mx-16">
+        <div
+          onClick={closeBubble}
+          className="nes-container nes-pointer bg-white/60 border-s-black rounded-full absolute -bottom-10 sm:-bottom-12 overflow-hidden max-w-[360px] mx-16"
+        >
           <Image
             src={HeroImage}
             alt="Hero Image"
@@ -20,6 +46,7 @@ const Hero: React.FC<HeroProps> = ({ title, subtitle }) => {
             style={{ imageRendering: "pixelated" }}
           />
         </div>
+        {textBubble && <CompanyTag onClick={closeBubble} />}
       </div>
       <h1 className="flex nes-badge absolute -bottom-8 sm:-bottom-10 text-2xl sm:text-3xl z-20">
         <span className="is-primary nes-text left-0 right-0 bottom-0 top-0 self-center">
