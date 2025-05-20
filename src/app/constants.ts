844b87c76757019e1f193df1ec0f4fa2a2178f93
Joshua Owens <38735622+joshbeme@users.noreply.github.com>
2025-05-20 03:40:20 -0700

    Removed const

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidV4 } from "uuid";
 
 // Accordion IDs
-export const EXPERIENCE_ACCORDION_ID = uuidV4() as const;
+export const EXPERIENCE_ACCORDION_ID = uuidV4();
 export const SKILLS_ACCORDION_ID = uuidV4();
