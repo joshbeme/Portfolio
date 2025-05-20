3ba80ae74b9290d32fbac720c5b3088cea3641e0
Joshua Owens <38735622+joshbeme@users.noreply.github.com>
2025-05-20 03:43:30 -0700

    Static id strings

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,3 @@
-"use client";
-import { v4 as uuidV4 } from "uuid";
-
 // Accordion IDs
-export const EXPERIENCE_ACCORDION_ID = uuidV4();
-export const SKILLS_ACCORDION_ID = uuidV4();
+export const EXPERIENCE_ACCORDION_ID = "experience-accordion";
+export const SKILLS_ACCORDION_ID = "skills-accordion";
